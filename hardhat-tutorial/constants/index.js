545064ca828e4f78ff3825954a08e48793cf5c47
Arian Hojat <armyofda12mnkeys@gmail.com>
2023-01-12 07:04:59 +0000

    ICO initial commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = '0x1F3833391FFD8Ea34b298CAbdAEAD2d68850188b'; //old one: "0x9feaeb09b1eae559025D90190E55cA5ff1bC488D";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
